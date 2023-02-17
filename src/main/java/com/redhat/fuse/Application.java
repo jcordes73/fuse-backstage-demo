340464a41cf57db53a4eb3756f4e6cd2a01654db
Scaffolder <scaffolder@backstage.io>
2023-02-17 13:46:20 +0000

    initial commit

---
+++ src/main/java/com/redhat/fuse/Application.java
@@ -0,0 +1,20 @@
+package com.redhat.fuse;
+
+import org.apache.camel.opentracing.starter.CamelOpenTracing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource({"classpath:spring/camel-context.xml"})
+@CamelOpenTracing
+public class Application {
+
+    /**
+     * A main method to start this application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
